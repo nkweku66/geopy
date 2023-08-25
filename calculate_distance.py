a9165cd8c6cebf109612baa64de391a269db45a5
denakwob <125455377+denakwob@users.noreply.github.com>
2023-08-25 04:44:44 +0000

    handle errors and round miles

--- calculate_distance.py
+++ calculate_distance.py
@@ -4,19 +4,25 @@ from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 
 geolocator =  Nominatim(user_agent="disc")
-x =  input("Name of first city: ")
-y = input("Name of second city: ")
 
-location1 = geolocator.geocode(x)
-location2 = geolocator.geocode(y)
-location1Lat = (location1.latitude, location1.longitude)
-location2Lat = (location2.latitude, location2.longitude)
-measure = geodesic(location1Lat, location2Lat).miles
-
-print(f"The distance between the cities is {measure} miles")
-#print(location2.latitude, location2.longitude)
-#def city_name(city1, city2):
+def calculate(location1, location2):
+    location1Lat = (location1.latitude, location1.longitude)
+    location2Lat = (location2.latitude, location2.longitude)
+    measure = int(geodesic(location1Lat, location2Lat).miles)
+    print(f"The distance between the cities is {measure} miles")
 
 
+while True:
+    try:
+        x = input("Name of first city: ")
+        y = input("Name of second city: ")
+        location1 = geolocator.geocode(x)
+        location2 = geolocator.geocode(y)
 
-#print(geodesic(x,y).miles)
+        if location1 is None or location2 is None:
+            print("Invalid input. Please provide valid city names.")
+        else:
+            calculate(location1, location2)
+            break
+    except Exception as e:
+        print("An error occured", e,"\n")

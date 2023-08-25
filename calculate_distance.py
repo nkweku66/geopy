7f4e97ffb1f6ed25bb774501151528861d56db8a
nkweku66 <nkweku66@gmail.com>
2023-08-25 16:31:15 +0100

    link geopy_api request to rasberrypi

--- calculate_distance.py
+++ calculate_distance.py
@@ -1,14 +1,21 @@
 #Authors: Nana Obeng, Oliver Quaye and Saani Mustapha
-
+from gpiozero import Button, LED
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
+import math
+
+led = LED(21)
+led1 = LED(20)
+button = Button(2)
 
 geolocator =  Nominatim(user_agent="disc")
 
 def calculate(location1, location2):
+    print("Please press the button to calculate")
+    button.wait_for_press()
     location1Lat = (location1.latitude, location1.longitude)
     location2Lat = (location2.latitude, location2.longitude)
-    measure = int(geodesic(location1Lat, location2Lat).miles)
+    measure = math.ceil(geodesic(location1Lat, location2Lat).miles)
     print(f"The distance between the cities is {measure} miles")
 
 
@@ -20,8 +27,11 @@ while True:
         location2 = geolocator.geocode(y)
 
         if location1 is None or location2 is None:
+            led.blink(0.5, 0.5)
             print("Invalid input. Please provide valid city names.")
         else:
+            led.off()
+            led1.on()
             calculate(location1, location2)
             break
     except Exception as e:

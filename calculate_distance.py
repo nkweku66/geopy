d55d22cb111d4ba0225670b1b7a49a726e541ec9
nkweku66 <nkweku66@gmail.com>
2023-08-27 08:08:06 +0100

    Make the usage much better and add feedbacks to inputs and actions performed by user

--- calculate_distance.py
+++ calculate_distance.py
@@ -1,38 +1,35 @@
 #Authors: Nana Obeng, Oliver Quaye and Saani Mustapha
-from gpiozero import Button, LED
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 import math
 
-led = LED(21)
-led1 = LED(20)
-button = Button(2)
-
 geolocator =  Nominatim(user_agent="disc")
 
 def calculate(location1, location2):
-    print("Please press the button to calculate")
-    button.wait_for_press()
     location1Lat = (location1.latitude, location1.longitude)
     location2Lat = (location2.latitude, location2.longitude)
     measure = math.ceil(geodesic(location1Lat, location2Lat).miles)
-    print(f"The distance between the cities is {measure} miles")
+    print(f"The distance between the cities is {measure} miles\n")
 
 
 while True:
     try:
+        print("Welcome to the distance calculator.Please type in:")
         x = input("Name of first city: ")
         y = input("Name of second city: ")
         location1 = geolocator.geocode(x)
         location2 = geolocator.geocode(y)
 
         if location1 is None or location2 is None:
-            led.blink(0.5, 0.5)
             print("Invalid input. Please provide valid city names.")
         else:
-            led.off()
-            led1.on()
             calculate(location1, location2)
-            break
+           # break
+            y = (input("Do you want to do another calculation? ")).lower()
+            if y == 'yes':
+                calculate(location1, location2)
+            else:
+                print("I am happy I was helpful to you :)")
+                break
     except Exception as e:
         print("An error occured", e,"\n")
